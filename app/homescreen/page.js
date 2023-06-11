a3328667b1f2851b61e9056ff70abd106c0023dc
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-06-11 23:51:54 +0530

    recent commit

--- app/homescreen/page.js
+++ app/homescreen/page.js
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "../../firebase";
 
 export default function home() {
   const router = useRouter();
