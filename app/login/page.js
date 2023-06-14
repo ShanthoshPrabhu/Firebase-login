e5f98a0582865e64bbe2123fea24632795ad583e
ShanthoshPrabhu <ashanthoshprabhu@gmail.com>
2023-06-14 10:36:13 +0530

    page fix

--- app/login/page.js
+++ app/login/page.js
@@ -14,7 +14,6 @@ function Login() {
     const [emailValid,setEmailValid]=useState(false);
     // const [ user,setUser] = useState([])
 //
-console.log('prov',providers)
 async function signIn () {
     
     signInWithEmailAndPassword(auth, email, password)
@@ -33,22 +32,7 @@ async function signIn () {
         <div className=' flex justify-center items-center h-screen'>
 
         <div className=' flex flex-col space-y-6 p-4 w-[360px] '>
-            {/* <div className="">
-                {Object?.values(providers).map((provider) => (
-                    <div key={provider.name}>
-                        
-                        <button
-                        className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group mt-[150px]"
-                        onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-                        >
-                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#ff9933] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                        <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                            Sign in with {provider.name}
-                        </span>
-                        </button>
-                    </div>
-                    ))}
-            </div> */}
+            
             {emailValid ? (
                 <div className=' w-[100%]'>
                     <label htmlFor="password" className='text-lg'>Password</label>
@@ -80,11 +64,3 @@ async function signIn () {
 export default Login
 
 
-export async function getServerSideProps(context) {
-    const providers = await getProviders(context)
-    return {
-      props: {
-        providers
-      },
-    }
-  }
