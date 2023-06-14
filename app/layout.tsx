5a12caf23d9d324c2e3595a168d5150d13bdb05b
ShanthoshPrabhu <ashanthoshprabhu@gmail.com>
2023-06-14 17:29:11 +0530

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,36 +24,3 @@ export default function RootLayout({
   )
 }
 
-// import { Session } from 'next-auth'
-// import { headers } from 'next/headers'
-// import AuthContext from './AuthContext';
-
-// async function getSession(cookie: string): Promise<Session> {
-//   const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, {
-//     headers: {
-//       cookie,
-//     },
-//   });
-
-//   const session = await response.json();
-
-//   return Object.keys(session).length > 0 ? session : null;
-// }
-
-// export default async function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode,
-// }) {
-//   const session = await getSession(headers().get('cookie') ?? '');
-//   return (
-//     <html lang="en">
-//       <head />
-//       <body>
-//         <AuthContext session={session}>
-//           {children}
-//         </AuthContext>
-//       </body>
-//     </html>
-//   )
-// }
